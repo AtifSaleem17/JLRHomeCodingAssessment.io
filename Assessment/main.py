c074e589b7d49872462fb9bde97f64e86e5f1f59
Muhammad Atif <67201977+AtifSaleem17@users.noreply.github.com>
2023-09-23 14:42:29 +0100

    a

--- Assessment/main.py
+++ Assessment/main.py
@@ -1,12 +1,6 @@
 # importing the ergast API module after installation
 import ergast_py
 
-
-# sorting the list with respect to points
-def sorting_points(result):
-    return result.results[0].points
-
-
 def best_performances():
     # initialise the object/instance of the Ergast class in order to access the API data
     ergast = ergast_py.Ergast()
@@ -21,14 +15,14 @@ def best_performances():
         constructors_list.append(constructors_2023[x].constructor_id)
     # fetching data of each constructor of 2023 for all the seasons (2023 - 1950)
     for constructor in constructors_list:  # loop for 2023 constructors
-        for season in range(2023, 1949, -1):  # loop for seasons
+        for season in range(2023, 2019, -1):  # loop for seasons
             try:
                 # building the query with function calls to fetch 2023 constructors' results
                 results = ergast.season(season).constructor(constructor).get_results()
                 # only if constructor raced in the season
                 if len(results) != 0:
                     # selecting only highest performance results after sorting in descending order
-                    results.sort(key=sorting_points, reverse=True)
+                    results.sort(key=lambda results : results.results[0].points, reverse=True)
                     # inserting the constructors and their corresponding values
                     if constructor not in performances:
                         performances[constructor] = [results[0].results[0].constructor.name,
@@ -45,9 +39,8 @@ def best_performances():
                             # updating season
                             performances[constructor][3] = season
 
-            except KeyError or ValueError:
-                print("ERROR: ", constructor.title(), "'s data not found for season ", season)
-            except ValueError:
+            except Exception as e:
+                print(e.args[0])
                 print("ERROR: ", constructor.title(), "'s data not found for season ", season)
 
     return performances
